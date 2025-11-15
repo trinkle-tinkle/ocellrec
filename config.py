955f74bb6826da3d025bbe21561863094bccb0ac
antonicas <188374813+trinkle-tinkle@users.noreply.github.com>
2025-11-15 10:52:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ SKIP_RECORDING = False # The recording will be skipped but the event file and in
 MIN_FILE_SIZE = 50000
 
 # Time range to check for the stream
-TRY_START_REC = "20:20" # May start late depending on CHECK_EVENT_DELAY
+TRY_START_REC = "20:15" # May start late depending on CHECK_EVENT_DELAY
 TRY_END_REC = "23:59"
 
 # Delay between attempts to record in seconds.
-REC_DELAY = 60
-CHECK_EVENT_DELAY = 600
+REC_DELAY = 40
+CHECK_EVENT_DELAY = 600 # This should be less than 20:30 - TRY_START_REC so that the recording is not late.
 
 # Log
 PRINT_TO_TERMINAL = True
